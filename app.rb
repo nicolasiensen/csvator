b26ecb225ee8fa45f755b2c5f595da3318009886
Nícolas Iensen <nicolas@engage.is>
2012-10-05 18:42:33 -0300

    Union, intersection and subtraction operations. You can also set the file name

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'slim'
+require 'csv'
+
+get '/' do
+  slim :index
+end
+
+post '/generator' do
+  headers "Content-Disposition" => "attachment;filename=#{params['name'] == "" ? "generator" : params['name'].gsub(" ", "_")}.csv", "Content-Type" => "application/octet-stream"
+  list1 = CSV.parse(params['list1'][:tempfile].read)
+  list2 = CSV.parse(params['list2'][:tempfile].read)
+  result = "Nome, Email\n"
+  eval("#{params['operation']}(list1, list2)").each do |item|
+    result << "#{item[0]}, #{item[1]}\n"
+  end
+  result
+end
+
+def union list1, list2
+  list1 | list2
+end
+
+def intersection list1, list2
+  list1 & list2
+end
+
+def subtraction list1, list2
+  list1 - list2
+end
